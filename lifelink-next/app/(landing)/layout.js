b3b1146a0d4d102ab00984634a9f5a3162196906
rnzlndr <renzlander@gmail.com>
2023-09-16 18:00:45 +0800

    Registration form 1 UI updates

--- lifelink-next/app/(landing)/layout.js
+++ lifelink-next/app/(landing)/layout.js
@@ -5,7 +5,9 @@ import { NavbarIndex } from './components/navbar';
 export default function RootLayout({ children }) {
   return (
       <body className='bg_lifelink'>
+        <nav className='px-2 w-full'>
         <NavbarIndex />
+        </nav>
         {children}
       </body>
   )
