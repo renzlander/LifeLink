d9c558d06560731d30e54ec424807d3ea1c53d1c
ryanjay <ryanjayantonio307@gmail.com>
2023-09-24 01:36:00 +0800

    Add Blood Bag Validation

--- lifelink-next/app/(auth)/login/components/loginForm.js
+++ lifelink-next/app/(auth)/login/components/loginForm.js
@@ -54,26 +54,26 @@ export function LoginForm() {
         Enter your Log In details.
       </Typography>
       {errorMessage && (
-  <div className="mt-4 text-center bg-red-100 p-2 rounded-lg">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="red"
-      className="w-4 h-4 mx-auto mb-1"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
-      />
-    </svg>
-    <Typography color="red" className="text-sm font-semibold">
-      {errorMessage}
-    </Typography>
-  </div>
-)}
+        <div className="mt-4 text-center bg-red-100 p-2 rounded-lg">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="red"
+            className="w-4 h-4 mx-auto mb-1"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8"
+            />
+          </svg>
+          <Typography color="red" className="text-sm font-semibold">
+            {errorMessage}
+          </Typography>
+        </div>
+      )}
 
 
       <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96" onSubmit={handleLogin}>
