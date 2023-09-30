39e835c805a4213315b2925f06cdd92a13abae29
rnzlndr <renzlander@gmail.com>
2023-09-30 17:44:49 +0800

    UI for User Profile

--- lifelink-next/app/(u_page)/u_profile/page.js
+++ lifelink-next/app/(u_page)/u_profile/page.js
@@ -1,25 +1,15 @@
 'use client'
 import React from "react";
-import Image from 'next/image';
-import { useRouter } from 'next/navigation';
-import {
-  Collapse,
-  Button,
-  Card,
-  Typography,
-  CardBody,
-} from "@material-tailwind/react";
-import CollapseDefault from "./components/collapse";
+import CardProfile, { CardInfo, CardDisplays } from "./components/cards";
 
 export default function Home() {
-  const router = useRouter();
-  const [open, setOpen] = React.useState(false);
- 
-  const toggleOpen = () => setOpen((cur) => !cur);
-
   return (
-    <div className="flex min-h-screen flex-col py-2 ml-72">
-      <CollapseDefault />
+    <div className="flex min-h-screen flex-col py-2 gap-y-3">
+      <CardProfile />
+      <div className="flex gap-3">
+        <CardInfo />
+        <CardDisplays />
+      </div>
     </div>
   )
 }
