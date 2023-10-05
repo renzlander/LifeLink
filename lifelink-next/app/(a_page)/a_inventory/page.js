80b2e376d461122cf3eedb354de429791cea5753
rnzlndr <renzlander@gmail.com>
2023-10-05 12:37:59 +0800

    Updated the table in Blood Inventory

--- lifelink-next/app/(a_page)/a_inventory/page.js
+++ lifelink-next/app/(a_page)/a_inventory/page.js
@@ -2,13 +2,13 @@
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { Button } from "@material-tailwind/react";
-import { InventoryTabs } from './components/tabs';
+import { InventoryTable } from './components/table';
 
 export default function Home() {
   const router = useRouter();
   return (
-    <div className="bg-gray-200 flex min-h-screen flex-col items-center justify-between py-4">
-      <InventoryTabs />
+    <div className="bg-gray-200 flex min-h-screen flex-col items-center justify-between p-12">
+      <InventoryTable />
     </div>
   )
 }
