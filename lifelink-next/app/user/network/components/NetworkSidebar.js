d1e2cb0b9b7b03b145785744d19a8fa15c571989
ryanjay <ryanjayantonio307@gmail.com>
2023-11-28 02:32:21 +0800

    user network done na ata?

--- lifelink-next/app/user/network/components/NetworkSidebar.js
+++ lifelink-next/app/user/network/components/NetworkSidebar.js
@@ -2,73 +2,137 @@ import React, { useEffect, useState } from "react";
 import { Card, CardBody, Typography, Chip } from "@material-tailwind/react";
 
 function formatDateTime(dateTimeString) {
-    const options = { year: "numeric", month: "long", day: "numeric", hour: "2-digit", minute: "2-digit", second: "2-digit" };
-    const formattedDateTime = new Date(dateTimeString).toLocaleDateString(undefined, options);
-    return formattedDateTime;
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  };
+  const formattedDateTime = new Date(dateTimeString).toLocaleDateString(
+    undefined,
+    options
+  );
+  return formattedDateTime;
 }
 
-export function RecentRequest({ latestBloodRequest }) {
+function hideMiddleCharacters(text) {
+  if (text && text.length && text.length > 2) {
+    const hiddenPart = "*".repeat(text.length - 2);
+    return text[0] + hiddenPart + text.slice(-1);
+  } else {
+    return text;
+  }
+}
+function formatDate(dateString) {
+  const options = { year: "numeric", month: "long", day: "numeric" };
+  const formattedDate = new Date(dateString).toLocaleDateString(
+    undefined,
+    options
+  );
+  return formattedDate;
+}
 
-    const chipColor = [
-        {color: "green", value: "Granted ", text: "Granted "},
-        {color: "red", value: "Declined ", text: "Declined "},
-        {color: "gray", value: "Pending", text: "Pending"},
-      ];
-    return (
-        <Card className="w-full">
-          <CardBody className="px-6 py-4">
-            {latestBloodRequest ? (
-              <div className="flex items-start justify-between w-full">
-                <div className="flex flex-col items-start justify-between w-4/6 gap-3">
-                  <Typography variant="h6">
-                    No. of units: 
-                    <spam className="font-normal ml-3">{latestBloodRequest.blood_units}</spam>
-                  </Typography>
-                  <Typography variant="h6">
-                      Blood Component: 
-                      <spam className="font-normal ml-3">{latestBloodRequest.blood_component_desc}</spam>
-                  </Typography>
-                  <Typography variant="h6">
-                    Hospital: 
-                    <spam className="font-normal ml-3">{latestBloodRequest.hospital}</spam>
-                  </Typography>
-                  <Typography variant="h6">
-                    Diagnosis: 
-                    <spam className="font-normal ml-3">{latestBloodRequest.diagnosis}</spam>
-                  </Typography>
-                  <Typography variant="h6">
-                    Schedule of Transfusion/Operation: 
-                    <spam className="font-normal ml-3">{formatDateTime(latestBloodRequest.schedule)}</spam>
-                  </Typography>
-                </div>
-                <div className="flex flex-col items-end w-1/3 gap-3">
-                  <Typography variant="small" color="blue-gray" className="text-md">
-                    Requested on 11/11/2023 at 20:00:00
-                  </Typography>
+export function RecentRequest({ bloodRequestHistory }) {
+  const chipColor = [
+    { color: "green", value: "Granted", text: "Granted" },
+    { color: "orange", value: "Referred", text: "Referred" },
+    { color: "gray", value: "Pending", text: "Pending" },
+    { color: "red", value: "Cancelled", text: "Cancelled" },
+  ];
+
+  return (
+    <div className="w-full">
+      {bloodRequestHistory && bloodRequestHistory.length > 0 ? (
+        bloodRequestHistory.map((request, index) => (
+          <Card key={index} className="w-full mb-4 shadow-lg">
+            <CardBody className="px-6 py-4 w-full">
+              <div className="flex flex-col w-full">
+                <div className="flex flex-col justify-between lg:flex-row sm:flex-row items-center w-full">
                   <Chip
                     variant="ghost"
                     size="lg"
+                    className="w-auto"
                     color={
-                      latestBloodRequest.isAccommodated === 0
+                      request.isAccommodated === 0
                         ? chipColor[2].color
-                        : latestBloodRequest.isAccommodated === 1
+                        : request.isAccommodated === 1
                         ? chipColor[0].color
-                        : chipColor[1].color
+                        : request.isAccommodated === 2
+                        ? chipColor[1].color
+                        : chipColor[3].color
                     }
                     value={
-                      latestBloodRequest.isAccommodated === 0
+                      request.isAccommodated === 0
                         ? chipColor[2].text
-                        : latestBloodRequest.isAccommodated === 1
+                        : request.isAccommodated === 1
                         ? chipColor[0].text
-                        : chipColor[1].text
+                        : request.isAccommodated === 2
+                        ? chipColor[1].text
+                        : chipColor[3].text
                     }
                   />
+                  <Typography
+                    variant="small"
+                    color="blue-gray"
+                    className="text-md mt-2"
+                  >
+                    Requested on {formatDateTime(request.created_at)}
+                  </Typography>
+                </div>
+                <hr className="my-4"></hr>
+                <div className="flex flex-col items-start w-full mb-3">
+                  <Typography variant="h6" className="text-black font-bold">
+                    No. of Units:
+                    <span className="font-normal ml-3 text-gray-700">
+                      {request.blood_units}
+                    </span>
+                  </Typography>
+                  <Typography variant="h6" className="text-black font-bold">
+                    Blood Component:
+                    <span className="font-normal ml-3 text-gray-700">
+                      {request.blood_component_desc}
+                    </span>
+                  </Typography>
+                  <Typography variant="h6" className="text-black font-bold">
+                    Hospital:
+                    <span className="font-normal ml-3 text-gray-700">
+                      {request.hospital}
+                    </span>
+                  </Typography>
+                  <Typography variant="h6" className="text-black font-bold">
+                    Diagnosis:
+                    <span className="font-normal ml-3 text-gray-700">
+                      {request.diagnosis}
+                    </span>
+                  </Typography>
+                  <Typography variant="h6" className="text-black font-bold">
+                    Schedule of Transfusion/Operation:
+                    <span className="font-normal ml-3 text-gray-700">
+                      {formatDate(request.schedule)}
+                    </span>
+                  </Typography>
+                  {request.isAccommodated === 3 && (
+                    <Typography className="text-black font-bold my-4">
+                      Reason: {request.cancel_reason}
+                    </Typography>
+                  )}
+                  {request.isAccommodated === 2 && (
+                    <Typography className="text-black font-bold my-4">
+                      Remarks: {request.remarks}
+                    </Typography>
+                  )}
                 </div>
               </div>
-            ) : (
-              <Typography>No Ongoing Request</Typography>
-            )}
-          </CardBody>
-        </Card>
-    );
+            </CardBody>
+          </Card>
+        ))
+      ) : (
+        <Typography className="text-gray-700 text-xl font-semibold text-center">
+          No Ongoing Requests
+        </Typography>
+      )}
+    </div>
+  );
 }
--- lifelink-next/app/user/network/components/Popup.js
+++ lifelink-next/app/user/network/components/Popup.js
@@ -1,206 +1,260 @@
 import React, { useState } from "react";
 import {
-    Card,
-    Checkbox,
-    Button,
-    IconButton,
-    SpeedDial,
-    SpeedDialHandler,
-    Dialog,
-    DialogHeader,
-    DialogBody,
-    DialogFooter,
-    Textarea,
-    List,
-    ListItem,
-    ListItemPrefix,
-    Typography,
+  Card,
+  Checkbox,
+  Button,
+  IconButton,
+  SpeedDial,
+  SpeedDialHandler,
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
+  Textarea,
+  List,
+  ListItem,
+  ListItemPrefix,
+  Typography,
 } from "@material-tailwind/react";
 import { PlusIcon } from "@heroicons/react/24/outline";
 import { ToastContainer, toast } from "react-toastify";
 import axios from "axios";
 import { laravelBaseUrl } from "@/app/variables";
 
-  const formatDate = (donationDate) => {
-    const formattedDate = new Date(donationDate).toLocaleString("en-US", {
-        month: "long",
-        day: "numeric",
-        year: "numeric",
-        hour: "2-digit",
-        minute: "2-digit",
-        hour12: true,
-    });
-    return formattedDate;
+const formatDate = (donationDate) => {
+  const formattedDate = new Date(donationDate).toLocaleString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+  });
+  return formattedDate;
 };
 
-  export function Interested({ requestId, bloodNeed, onInterestedClick, updateInterestedBloodRequests, fetchMySchedule }) {
-    const [open, setOpen] = useState(false);
-    const [generalErrorMessage, setGeneralErrorMessage] = useState("");
-    const handleInterested = async () => {
-      try {
-          const token = getCookie("token");
-          if (!token) {
-              router.push("/login");
-              return;
-          }
-
-          // Prepare data for the POST request
-          const data = {
-              blood_request_id:requestId,
-            //   blood_type_need:bloodNeed
-          };
-
-          const response = await axios
-              .post(`${laravelBaseUrl}/api/button-interested`, data, {
-                  headers: {
-                      Authorization: `Bearer ${token}`,
-                  },
-              })
-              .catch((error) => {
-                  console.error("Unknown error occurred:", error);
-                  if (error.response && error.response.data && error.response.data.errors) {
-                    setGeneralErrorMessage(error.response.data.message);
-                    toast.error("Opps! Something went wrong.");
-                  } else {
-                    setGeneralErrorMessage(error.response.data.message);
-                    toast.error("Opps! Something went wrong.");                      }
-              });
-
-          if (response.data.status === "success") {
-                toast.success("Thank you for your interest!");
-                updateInterestedBloodRequests(requestId);
-                fetchMySchedule();
-                setOpen(false);
-          } else if (response.data.status === "error") {
-              if (response.data.message) {
-                  setGeneralErrorMessage(response.data.message);
-              } else {
-                  console.error("Unknown error occurred:", response.data);
-              }
-          }
-
-      } catch (error) {
-          toast.error(error);
-          console.error("Unknown error occurred:", error);
+export function Interested({
+  requestId,
+  bloodNeed,
+  onInterestedClick,
+  updateInterestedBloodRequests,
+  fetchMySchedule,
+}) {
+  const [open, setOpen] = useState(false);
+  const [generalErrorMessage, setGeneralErrorMessage] = useState("");
+  const handleInterested = async () => {
+    try {
+      const token = getCookie("token");
+      if (!token) {
+        router.push("/login");
+        return;
       }
+
+      // Prepare data for the POST request
+      const data = {
+        blood_request_id: requestId,
+        patient_blood_type:bloodNeed
+      };
+
+      const response = await axios
+        .post(`${laravelBaseUrl}/api/button-interested`, data, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .catch((error) => {
+          console.error("Unknown error occurred:", error);
+          if (
+            error.response &&
+            error.response.data &&
+            error.response.data.errors
+          ) {
+            setGeneralErrorMessage(error.response.data.message);
+            toast.error("Opps! Something went wrong.");
+          } else {
+            setGeneralErrorMessage(error.response.data.message);
+            toast.error("Opps! Something went wrong.");
+          }
+        });
+
+        console.log("Dasdasdsa", response);
+      if (response.data.status === "success") {
+        toast.success("Thank you for your interest!");
+        updateInterestedBloodRequests(requestId);
+        fetchMySchedule();
+        setOpen(false);
+      } else if (response.data.status === "error") {
+        if (response.data.message) {
+          setGeneralErrorMessage(response.data.message);
+        } else {
+          console.error("Unknown error occurred:", response.data);
+        }
+      }
+    } catch (error) {
+      toast.error(error);
+      console.error("Unknown error occurred:", error);
+    }
   };
 
-    return (
-      <>
-          <Button color="green" variant="gradient" className="w-1/8 mt-4" onClick={() => setOpen(true)}>
-              <span>I'm Interested</span>
+  return (
+    <>
+      <Button
+        color="green"
+        variant="gradient"
+        className="w-1/8 mt-4"
+        onClick={() => setOpen(true)}
+      >
+        <span>I'm Interested</span>
+      </Button>
+      <Dialog open={open} handler={() => setOpen(false)}>
+        <DialogHeader className="bg-gradient-to-r from-[rgba(40,40,40,1)] to-[rgba(160,12,8,1)] text-white font-semibold">
+          Confirmation of Interest
+        </DialogHeader>
+        {generalErrorMessage && (
+          <div className="mt-2 text-center bg-red-100 p-2 rounded-lg">
+            <Typography color="red" className="text-sm font-semibold">
+              {generalErrorMessage}
+            </Typography>
+          </div>
+        )}
+        <DialogBody divider className="flex flex-col gap-4 items-center">
+          <Typography className="text-sm text-blue-gray-500 text-center">
+            Note: By confirming your interest, Red Cross staff may contact you
+            for further details. Your contact information will be used solely
+            for donation coordination. Donating blood is voluntary, free, and
+            not compulsory. We appreciate your willingness to contribute to this
+            life-saving cause.
+          </Typography>
+          <Typography className="font-semibold text-sm text-red-600 text-center">
+            Are you sure you would like to express your interest in donating?
+          </Typography>
+        </DialogBody>
+        <DialogFooter className="flex justify-center mt-4">
+          <Button
+            variant="red-cross"
+            onClick={() => setOpen(false)}
+            className="mr-2"
+          >
+            No
           </Button>
-          <Dialog open={open} handler={() => setOpen(false)}>
-              <DialogHeader className="bg-gradient-to-r from-[rgba(40,40,40,1)] to-[rgba(160,12,8,1)] text-white font-semibold">
-                Confirmation of Interest
-              </DialogHeader>
-              {generalErrorMessage && (
-                <div className="mt-2 text-center bg-red-100 p-2 rounded-lg">
-                  <Typography color="red" className="text-sm font-semibold">
-                    {generalErrorMessage}
-                  </Typography>
-                </div>
-              )}
-              <DialogBody divider className="flex flex-col gap-4 items-center">
-                  <Typography className="text-sm text-blue-gray-500 text-center">
-                      Note: By confirming your interest, Red Cross staff may contact you for further details. Your contact information will be used solely for donation coordination. Donating blood is voluntary, free, and not compulsory. We appreciate your willingness to contribute to this life-saving cause.
-                  </Typography>
-                  <Typography className="font-semibold text-sm text-red-600 text-center">
-                    Are you sure you would like to express your interest in donating?
-                  </Typography>
-              </DialogBody>
-              <DialogFooter className="flex justify-center mt-4">
-                  <Button variant="red-cross" onClick={() => setOpen(false)} className="mr-2">
-                      No
-                  </Button>
-                  <Button variant="red-cross" color="red" onClick={handleInterested}>
-                      Yes, Confirm Interest
-                  </Button>
-              </DialogFooter>
-          </Dialog>
-      </>
+          <Button variant="red-cross" color="red" onClick={handleInterested}>
+            Yes, Confirm Interest
+          </Button>
+        </DialogFooter>
+      </Dialog>
+    </>
   );
 }
 
-export function CancelRequest({}){
-    const [open, setOpen] = useState(false);
-    const [generalErrorMessage, setGeneralErrorMessage] = useState("");
+export function CancelRequest({}) {
+  const [open, setOpen] = useState(false);
+  const [reason, setReason] = useState("");
+  const [generalErrorMessage, setGeneralErrorMessage] = useState("");
 
-    const handleCancelRequest = async () => {
-      try {
-          const token = getCookie("token");
-          if (!token) {
-              router.push("/login");
-              return;
-          }
-
-          const response = await axios
-              .get(`${laravelBaseUrl}/api/cancel-request-blood`, {
-                  headers: {
-                      Authorization: `Bearer ${token}`,
-                  },
-              })
-              .catch((error) => {
-                  console.error("Unknown error occurred:", error);
-                  if (error.response && error.response.data && error.response.data.errors) {
-                    toast.error("Opps! Something went wrong.");
-                  } else {
-                    setGeneralErrorMessage(error.response.data.message);
-                  }
-              });
-
-          if (response.data.status === "success") {
-                toast.success("Blood request cancelled successfully");
-                window.location.reload();
-                setOpen(false);
-          } else if (response.data.status === "error") {
-              if (response.data.message) {
-                  setGeneralErrorMessage(response.data.message);
-              } else {
-                  console.error("Unknown error occurred:", response.data);
-              }
-          }
-
-      } catch (error) {
-          toast.error(error);
-          console.error("Unknown error occurred:", error);
+  const handleCancelRequest = async () => {
+    try {
+      const token = getCookie("token");
+      if (!token) {
+        router.push("/login");
+        return;
       }
+
+      const response = await axios
+        .post(
+          `${laravelBaseUrl}/api/cancel-request-blood`,
+          {
+            reason: reason,
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          }
+        )
+        .catch((error) => {
+          console.error("Unknown error occurred:", error);
+          if (
+            error.response &&
+            error.response.data &&
+            error.response.data.errors
+          ) {
+            toast.error("Oops! Something went wrong.");
+          } else {
+            setGeneralErrorMessage(error.response.data.message);
+          }
+        });
+
+      if (response.data.status === "success") {
+        toast.success("Blood request cancelled successfully");
+        window.location.reload();
+        setOpen(false);
+      } else if (response.data.status === "error") {
+        if (response.data.message) {
+          setGeneralErrorMessage(response.data.message);
+        } else {
+          console.error("Unknown error occurred:", response.data);
+        }
+      }
+    } catch (error) {
+      toast.error(error);
+      console.error("Unknown error occurred:", error);
+    }
   };
 
-    return (
-      <>
-          <Button color="red" variant="gradient" className="w-full mt-2" onClick={() => setOpen(true)}>
-              <span>Cancel Request</span>
+  const handleBodyChange = (e) => {
+    setReason(e.target.value);
+  };
+
+  return (
+    <>
+      <Button
+        color="red"
+        variant="gradient"
+        className="rounded-full"
+        onClick={() => setOpen(true)}
+      >
+        Cancel Request
+      </Button>
+      <Dialog open={open} handler={() => setOpen(false)}>
+        <DialogHeader className="bg-gradient-to-r from-[rgba(40,40,40,1)] to-[rgba(160,12,8,1)] text-white font-semibold">
+          Confirmation of Cancellation
+        </DialogHeader>
+        {generalErrorMessage && (
+          <div className="mt-2 text-center bg-red-100 p-2 rounded-lg">
+            <Typography color="red" className="text-sm font-semibold">
+              {generalErrorMessage}
+            </Typography>
+          </div>
+        )}
+        <DialogBody divider className="flex flex-col gap-4 items-center">
+          <Textarea
+            size="lg"
+            label="Reason for Cancellation"
+            maxLength={500}
+            onChange={handleBodyChange}
+          />
+          <Typography className="text-sm text-blue-gray-500 text-center">
+            Note: By canceling your blood request, you are indicating that you
+            no longer need blood.
+          </Typography>
+          <Typography className="font-semibold text-sm text-red-600 text-center">
+            Are you sure you would like to cancel your last blood request
+          </Typography>
+        </DialogBody>
+        <DialogFooter className="flex justify-center mt-4">
+          <Button
+            variant="red-cross"
+            onClick={() => setOpen(false)}
+            className="mr-2"
+          >
+            No
           </Button>
-          <Dialog open={open} handler={() => setOpen(false)}>
-              <DialogHeader className="bg-gradient-to-r from-[rgba(40,40,40,1)] to-[rgba(160,12,8,1)] text-white font-semibold">
-                Confirmation of Cancellation
-              </DialogHeader>
-              {generalErrorMessage && (
-                <div className="mt-2 text-center bg-red-100 p-2 rounded-lg">
-                  <Typography color="red" className="text-sm font-semibold">
-                    {generalErrorMessage}
-                  </Typography>
-                </div>
-              )}
-              <DialogBody divider className="flex flex-col gap-4 items-center">
-                  <Typography className="text-sm text-blue-gray-500 text-center">
-                      Note: By canceling your blood request, you are indicating that you no longer need blood. The cancellation of request will no longer be available after 24 hours.
-                  </Typography>
-                  <Typography className="font-semibold text-sm text-red-600 text-center">
-                    Are you sure you would like to cancel your last blood request
-                  </Typography>
-              </DialogBody>
-              <DialogFooter className="flex justify-center mt-4">
-                  <Button variant="red-cross" onClick={() => setOpen(false)} className="mr-2">
-                      No
-                  </Button>
-                  <Button variant="red-cross" color="red" onClick={handleCancelRequest}>
-                      Confirm
-                  </Button>
-              </DialogFooter>
-          </Dialog>
-      </>
+          <Button variant="red-cross" color="red" onClick={handleCancelRequest}>
+            Confirm
+          </Button>
+        </DialogFooter>
+      </Dialog>
+    </>
   );
 }
 
