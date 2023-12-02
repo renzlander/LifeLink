678ae95c4afb1fdf832e991cfb21fa99d31df649
rnzlndr <renzlander@gmail.com>
2023-12-03 00:06:56 +0800

    -Settings: Moved Maintenance mode to the bottom

--- lifelink-next/app/admin/settings/page.js
+++ lifelink-next/app/admin/settings/page.js
@@ -1,16 +1,4 @@
 "use client";
-import { Card, List, ListItem, Typography } from "@material-tailwind/react";
-import { BledByCrud } from "./components/BledCrud";
-import { HospitalCrud } from "./components/HospitalsCrud";
-import { MaintenanceSwitch } from "./components/Maintenance";
-import { PDCatCrud } from "./components/PdcatCrud";
-import { SecurityPin } from "./components/SecurityPin";
-import { TDCatCrud } from "./components/TdcatCrud";
-import { ReactiveRemarksCrud } from "./components/ReactiveCrud";
-import { SpoiledRemarksCrud } from "./components/SpoiledCrud";
-
-import { VenueCrud } from "./components/VenueCrud";
-
 import HospitalIcon from "@/public/HospitalIcon";
 import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 import {
@@ -19,71 +7,81 @@ import {
   QueueListIcon,
   UserIcon,
 } from "@heroicons/react/24/solid";
+import { Card, List, ListItem } from "@material-tailwind/react";
+import { BledByCrud } from "./components/BledCrud";
+import { HospitalCrud } from "./components/HospitalsCrud";
+import { MaintenanceSwitch } from "./components/Maintenance";
+import { PDCatCrud } from "./components/PdcatCrud";
+import { ReactiveRemarksCrud } from "./components/ReactiveCrud";
+import { SecurityPin } from "./components/SecurityPin";
+import { SpoiledRemarksCrud } from "./components/SpoiledCrud";
+import { TDCatCrud } from "./components/TdcatCrud";
+import { VenueCrud } from "./components/VenueCrud";
 
 export default function Home() {
   const menu = [
     {
-      title: "Maintenance Mode",
-      icon: <ExclamationTriangleIcon className="h-5 w-5 text-black" />,
-      scroll: () => {
-        window.scrollTo({ top: 0, behavior: "smooth" });
-      }
-    },
-    {
       title: "Venues",
       icon: <HomeModernIcon className="h-5 w-5 text-black" />,
       scroll: () => {
-        window.scrollTo({ top: 215, behavior: "smooth" });
-      }
+        window.scrollTo({ top: 0, behavior: "smooth" });
+      },
     },
     {
       title: "Hospitals",
       icon: <HospitalIcon className="h-5 w-5" />,
       scroll: () => {
-        window.scrollTo({ top: 737, behavior: "smooth" });
-      }
+        window.scrollTo({ top: 523, behavior: "smooth" });
+      },
     },
     {
       title: "Bled By",
       icon: <UserIcon className="h-5 w-5 text-black" />,
       scroll: () => {
-        window.scrollTo({ top: 1260, behavior: "smooth" });
-      }
+        window.scrollTo({ top: 1047, behavior: "smooth" });
+      },
     },
     {
       title: "Temporary Deferral Category",
       icon: <QueueListIcon className="h-5 w-5 text-black" />,
       scroll: () => {
-        window.scrollTo({ top: 1790, behavior: "smooth" });
-      }
+        window.scrollTo({ top: 1570, behavior: "smooth" });
+      },
     },
     {
       title: "Permanent Deferral Category",
       icon: <QueueListIcon className="h-5 w-5 text-black" />,
       scroll: () => {
-        window.scrollTo({ top: 2313, behavior: "smooth" });
-      }
+        window.scrollTo({ top: 2101, behavior: "smooth" });
+      },
     },
     {
       title: "Reactive Blood Bags Remarks",
       icon: <QueueListIcon className="h-5 w-5 text-black" />,
       scroll: () => {
-        window.scrollTo({ top: 2836, behavior: "smooth" });
-      }
+        window.scrollTo({ top: 2624, behavior: "smooth" });
+      },
     },
     {
       title: "Spoiled Blood Bags Remarks",
       icon: <QueueListIcon className="h-5 w-5 text-black" />,
       scroll: () => {
-        window.scrollTo({ top: 3366, behavior: "smooth" });
-      }
+        window.scrollTo({ top: 3148, behavior: "smooth" });
+      },
     },
     {
       title: "Security Pin",
       icon: <KeyIcon className="h-5 w-5 text-black" />,
       scroll: () => {
-        window.scrollTo({ top: 3853, behavior: "smooth" });
-      }
+        window.scrollTo({ top: 3679, behavior: "smooth" });
+      },
+    },
+    {
+      title: "Maintenance Mode",
+      icon: <ExclamationTriangleIcon className="h-5 w-5 text-black" />,
+      scroll: () => {
+        window.scrollTo({ top: 3679, behavior: "smooth" });
+      },
     },
   ];
 
@@ -93,21 +91,18 @@ export default function Home() {
         <Card className="w-1/3 h-full overflow-auto sticky top-20">
           <List>
             {menu.map((item, index) => (
-              <Typography
+              <ListItem
                 key={index}
                 onClick={item.scroll}
-                className="scroll-smooth"
+                className="flex items-center gap-3"
               >
-                <ListItem className="flex items-center gap-3">
-                  {item.icon}
-                  {item.title}
-                </ListItem>
-              </Typography>
+                {item.icon}
+                {item.title}
+              </ListItem>
             ))}
           </List>
         </Card>
         <div className="w-full flex flex-col gap-4">
-          <MaintenanceSwitch />
           <VenueCrud />
           <HospitalCrud />
           <BledByCrud />
@@ -116,6 +111,7 @@ export default function Home() {
           <ReactiveRemarksCrud />
           <SpoiledRemarksCrud />
           <SecurityPin />
+          <MaintenanceSwitch />
         </div>
       </div>
     </div>
